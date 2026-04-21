8ea35569f2a263709a20d2823c076a3c4d11546c
Link4Electronics <link4electronics@gmail.com>
2026-04-21 12:17:53 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -23,7 +23,7 @@ case "$ARCH" in # they use AMD64 and ARM64 for the deb links
 	x86_64)  deb_arch=amd64;;
 	aarch64) deb_arch=arm64;;
 esac
-DEB_LINK=$(wget https://api.github.com/repos/shiftkey/desktop/releases -O - \
+DEB_LINK=$(wget https://api.github.com/repos/shiftkey/desktop/releases/latest -O - \
       | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*-$deb_arch-*.deb")
 echo "$DEB_LINK" | awk -F'/' '{gsub(/^v/, "", $(NF-1)); print $(NF-1); exit}' > ~/version
 if ! wget --retry-connrefused --tries=30 "$DEB_LINK" -O /tmp/app.deb 2>/tmp/download.log; then
