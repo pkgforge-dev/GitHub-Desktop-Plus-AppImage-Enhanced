d3231818529e68df9666c7044f8f709e3604f832
Link4Electronics <link4electronics@gmail.com>
2026-04-22 03:11:53 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,6 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export STARTUPWMCLASS=GitHub Desktop
-#export ALWAYS_SOFTWARE=1
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
