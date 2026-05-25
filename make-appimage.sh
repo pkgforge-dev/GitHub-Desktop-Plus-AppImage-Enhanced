f9b9cbbef106ee94409d0faddf28053577dd8cfd
Link4Electronics <link4electronics@gmail.com>
2026-05-25 20:39:47 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,15 +10,17 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export STARTUPWMCLASS=github-desktop-plus
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
+export DEPLOY_PYTHON=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/* \
   /usr/bin/git-lfs \
   /usr/bin/gnome* \
+  /usr/bin/pre-commit \
+  /usr/bin/secret-tool \
   /usr/lib/gnome-keyring/devel/gkm*.so* \
   /usr/lib/pkcs11/gnome*.so* \
   /usr/lib/security/pam*.so* \
-  /usr/bin/secret-tool \
   /usr/lib/libsecret*.so* \
   /usr/lib/libcurl*.so*
 
