8b34d5f2db71bf1637064c6849a598e84b2206e7
Link4Electronics <link4electronics@gmail.com>
2026-05-25 19:36:05 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+	git-lfs		   \
 	gnome-keyring  \
 	libcurl-gnutls
 
